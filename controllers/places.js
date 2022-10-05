177222175ffab1041fe7a4a83d0e049d726f979d
Adam Mattheis <oldcartoons@Adams-MBP.attlocal.net>
2022-10-05 02:39:38 -0400

    delete funct implemented

--- controllers/places.js
+++ controllers/places.js
@@ -40,5 +40,19 @@ router.post('/', (req, res) => {
   res.redirect('/places')
 })
 
+//delete
+router.delete('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.splice(id, 1)
+    res.redirect('/places')
+  }
+})
 
 module.exports = router
