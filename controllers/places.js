3df97fca9861214d59ba3818cc86d477de035ceb
Adam Mattheis <oldcartoons@Adams-MBP.attlocal.net>
2022-10-05 01:39:16 -0400

    show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -2,7 +2,6 @@ const places = require('../models/places')
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
-    
     res.render('places/index', {places})
 })
 
@@ -10,12 +9,22 @@ router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
-router.get('/places/:id', (req, res) => {
-  res.render('places/:id')
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places: places[id]')
+  }
 })
 
+
+
 router.post('/', (req, res) => {
-  console.log(req.body)
   if (!req.body.pic) {
     // Default image if one is not provided
     req.body.pic = 'http://placekitten.com/400/400'
