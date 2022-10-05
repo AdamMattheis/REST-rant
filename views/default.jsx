60fc6f0e0a63cbd5aac2aea540c9feb71560d984
Adam Mattheis <oldcartoons@Adams-MBP.attlocal.net>
2022-10-04 23:43:09 -0400

    adding css

--- views/default.jsx
+++ views/default.jsx
@@ -1,19 +1,20 @@
 const React = require('react')
-const Def = require('./default')
 
-function defa (html) {
+function Def (html) {
     return (
         <html>
             <head>
                 <title>Title</title>
+                <link rel="stylesheet" href="/css/style.css"/>            
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous" />
             </head>
             <body>
                 {html.children}
+                
             </body>
         </html>
     )
 }
-      
 
+module.exports = Def
 
-module.exports = defa
--- views/home.jsx
+++ views/home.jsx
@@ -3,13 +3,21 @@ const Def = require('./default')
 
 function home () {
     return (
-      <Def>
-          <main>
-              <h1>HOME</h1>
-          </main>
-      </Def>
+        <Def>
+            <main>
+                <h1>HOME</h1>
+                <div class='main'>
+                    <div>
+                    <img  src="/images/french-toast.jpg" alt="French Toast" />
+                        Photo by <a href="https://unsplash.com/@miracletwentyone">Joseph Gonzalez</a> on <a href="https://unsplash.com/">Unsplash</a>
+                    </div>
+                    <a href="/places">
+                        <button className="btn-primary">Places Page</button>
+                    </a>
+                  </div>
+            </main>
+        </Def>
     )
-  }
-  
+}
 
 module.exports = home
